e7f8e610520af6d79cd7d94d5fb00dae847d0532
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-12-13 20:57:38 -0300

    Create paramorder25.go

---
+++ paramorder25.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func formatParameters(lst []string) []string {
+	params := make([]string, 0)
+	line := ""
+	for i, item := range lst {
+		param := fmt.Sprintf("%s=FUZZ", item)
+		if i > 0 && i%25 == 0 {
+			params = append(params, line)
+			line = ""
+		}
+		if line != "" {
+			line += "&"
+		}
+		line += param
+	}
+	if line != "" {
+		params = append(params, line)
+	}
+	return params
+}
+
+func main() {
+	listFile := flag.String("l", "", "File containing the list")
+	flag.Parse()
+
+	if *listFile == "" {
+		fmt.Println("Please provide a list file using the -listfile flag.")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(*listFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lst []string
+	for scanner.Scan() {
+		lst = append(lst, scanner.Text())
+	}
+
+	params := formatParameters(lst)
+	for _, param := range params {
+		fmt.Printf("?%s\n", param)
+	}
+}
